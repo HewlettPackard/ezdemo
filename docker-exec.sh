91429c7d20a69817c3410a6df55c5b20da7cdaee
Erdinc Kaya <kaya@hpe.com>
2021-12-13 10:55:55 +0000

    rename scripts and change mapr instance type

--- docker-exec.sh
+++ docker-exec.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-CID=$(docker ps -f "status=running" -f "ancestor=erdincka/ezdemo" --format "{{ .ID }}")
+CID=$(docker ps -f "status=running" --format "{{ .ID }}")
 docker exec -it "${CID}" /bin/bash
 
 exit 0
