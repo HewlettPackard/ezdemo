2926fb9d9162f21c5c0e360d08d68dc42c65dd7e
Erdinc Kaya <kaya@hpe.com>
2021-12-15 16:00:01 +0000

    typo and path fixes

--- docker-copyfiles.sh
+++ docker-copyfiles.sh
@@ -4,5 +4,8 @@ set -euo pipefail
 # CID=$(docker ps -f "status=running" -f "ancestor=erdincka/ezdemo" --format "{{ .ID }}")
 CID=$(docker ps -f "status=running" --format "{{ .ID }}")
 docker cp "${CID}:/root/.hpecp_admin.config" ~/.kube/"${CID}"_admin.config 
+docker cp "${CID}:/root/.hpecp.conf" ~/"${CID}".hpecp.conf
+docker cp "${CID}:/root/.hpecp_tenant.conf" ~/"${CID}".hpecp_tenant.conf || true
+docker cp "${CID}:/app/server/generated/ca-cert.pem" ~/"${CID}".ca-cert.pem || true
 
 exit 0
