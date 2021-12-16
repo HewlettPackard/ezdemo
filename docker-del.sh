c08a6c87620c8afdcdd58a766770d18c33ef696b
Erdinc Kaya <kaya@hpe.com>
2021-12-16 23:28:19 +0000

    script update

--- docker-del.sh
+++ docker-del.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-CID=$(docker ps -f "status=running" -f "ancestor=erdincka/ezdemo" --format "{{ .ID }}")
+CID=$(docker ps -f "status=running" -f "ancestor=erdincka/ezdemo" -q)
 if [ "${CID}" != "" ]; then 
   rm -f ~/.kube/"${CID}"_admin.config 
   rm -f ~/"${CID}".hpecp.conf
@@ -10,5 +10,8 @@ if [ "${CID}" != "" ]; then
   rm -f ~/"${CID}".ca-cert.pem
   docker stop "${CID}" || true 
   docker rm "${CID}" || true 
+else
+  echo "No running image matched, are you looking for this?"
+  docker ps -f "status=running"
 fi 
 exit 0
