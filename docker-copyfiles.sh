9baf12652e6ec95eee3c9fcabb606a09d0e24748
Erdinc Kaya <kaya@hpe.com>
2021-12-17 11:49:08 +0000

    copy the right certificate file

--- docker-copyfiles.sh
+++ docker-copyfiles.sh
@@ -6,6 +6,6 @@ CID=$(docker ps -f "status=running" --format "{{ .ID }}")
 docker cp "${CID}:/root/.hpecp_admin.config" ~/.kube/"${CID}"_admin.config 
 docker cp "${CID}:/root/.hpecp.conf" ~/"${CID}".hpecp.conf
 docker cp "${CID}:/root/.hpecp_tenant.conf" ~/"${CID}".hpecp_tenant.conf || true
-docker cp "${CID}:/app/server/generated/ca-cert.pem" ~/"${CID}".ca-cert.pem || true
+docker cp "${CID}:/app/server/generated/minica.pem" ~/"${CID}".minica.pem || true
 
 exit 0
