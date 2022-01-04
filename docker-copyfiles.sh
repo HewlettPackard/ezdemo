3eb606ba429f04ec0ae33391c3c9432c75dbb2eb
Erdinc Kaya <kaya@hpe.com>
2022-01-04 11:43:09 +0000

    correction to docker id

--- docker-copyfiles.sh
+++ docker-copyfiles.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# CID=$(docker ps -f "status=running" -f "ancestor=erdincka/ezdemo" --format "{{ .ID }}")
-CID=$(docker ps -f "status=running" --format "{{ .ID }}")
+CID=$(docker ps -f "status=running" -f "ancestor=erdincka/ezdemo" -q)
+# CID=$(docker ps -f "status=running" --format "{{ .ID }}")
 docker cp "${CID}:/root/.hpecp_admin.config" ~/.kube/"${CID}"_admin.config 
 docker cp "${CID}:/root/.hpecp.conf" ~/"${CID}".hpecp.conf
 docker cp "${CID}:/root/.hpecp_tenant.conf" ~/"${CID}".hpecp_tenant.conf || true
