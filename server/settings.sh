5362b708f238488ca0df61a6d89dd99cd73525ad
Erdinc Kaya <kaya@hpe.com>
2022-01-05 13:44:15 +0000

    default to stable release

--- server/settings.sh
+++ server/settings.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-EPIC_DL_URL=$(jq -r '.epic_latest' ./system.settings)
+EPIC_DL_URL=$(jq -r '.epic_stable' ./system.settings)
 IS_HA=$(jq -r '.is_ha // false' ./system.settings)
 IS_RUNTIME=$(jq -r '.is_runtime // false' ./system.settings)
 # IS_MAPR=$(jq -r '.is_mapr // false' ./system.settings)
