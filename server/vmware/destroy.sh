326b7720bc8ba046db6b7459745475deac003dbf
Erdinc Kaya <erdincka@msn.com>
2022-05-09 21:41:45 +0100

    enable target specific destroy script

--- server/vmware/destroy.sh
+++ server/vmware/destroy.sh
@@ -21,51 +21,9 @@
 # SOFTWARE.
 # =============================================================================
 
-USAGE="Usage: ${0} $(paste -s -d '|' providers)"
-
-PROVIDERS=($(<providers))
-if ! [ $# -gt 0 ] || ! (echo ${PROVIDERS[@]} | grep -w -q ${1}); then
-  echo $USAGE
-  exit 1
-fi
 set -euo pipefail
 
-pushd "${1}" > /dev/null
-  TF_IN_AUTOMATION=1 terraform destroy ${EZWEB_TF:-} \
-    -parallelism 12 \
-    -var-file=<(cat ./*.tfvars) \
-    -auto-approve=true \
+rm hosts-common.ini
+rm hosts.ini
 
-   if [[ -f "./destroy.sh" ]]; then
-      "./destroy.sh"
-   fi
-
-popd > /dev/null
-
-(ls "${1}"/*run.log | xargs rm -f) || true
-(ls -d generated/*/ | xargs rm -rf) || true # Deletes all folders under generated, better than deleting the generated folder all together
-
-## Clean user environment
-rm -f ~/.hpecp.conf
-rm -f ~/.hpecp_tenant.conf
-rm -f ~/.kube/config
-
-source outputs.sh ${1}
-
-# If sockets are created for MCS
-if [[ "${IS_MAPR}" == "true" ]]
-then
-  for socket_file in "admin" "installer" "airflow" "kibana"
-  do
-    ssh -S /tmp/MCS-socket-${socket_file} -O exit centos@${GATW_PRV_DNS} || true
-  done
-fi
-
-rm -f generated/output.json
-rm -f ansible/group_vars/all.yml
-rm -f ansible/inventory.ini
-
-echo "Environment destroyed"
-echo "SSH key-pair, CA certs and cloud-init files are not removed!"
-
-exit 0
+echo "Ansible destroy needed"
